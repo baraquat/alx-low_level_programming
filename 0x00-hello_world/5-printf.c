4aa4ce652c40005931f592f1c4683dbc49dc0443
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-13 16:50:54 -0700

    read me

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- start function
+ * using printf
+ * return: zero if no errors
+ */
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
