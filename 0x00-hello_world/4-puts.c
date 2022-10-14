c0bf986bd4686a6aee059e40f70b7e3335d6bcec
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-14 15:11:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main- is the start function
  * description- puts instead of printf
  * return: zero value
- */
+ * */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
