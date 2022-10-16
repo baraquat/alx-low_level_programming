632016ec866b2bdead68a21e443804b4d99fe1f9
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-16 10:15:20 -0700

    print num less than 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all single digit of base 10 starting from 0
+ * use only putchar and not more than twice
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '10')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
