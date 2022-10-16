ef15a12742e20114825c7caff3fcb9851a64483f
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-16 16:26:49 -0700

    print 3

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of 3 digits
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (k = '0'; k <= '9'; k++)
+	{
+		for (j = (k + 1); j <= '9'; j++)
+		{
+			for (i = (j + 1); i <= '9'; i++)
+			{
+				putchar(k);
+				putchar(j);
+				putchar(i);
+				{
+					if (k != '7' || j != '8' || i != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
