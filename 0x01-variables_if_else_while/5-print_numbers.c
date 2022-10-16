30a825849091ad901826c374d52442c72081580e
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-16 10:02:45 -0700

    print numbers less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digits from 0 to 9
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10, i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
