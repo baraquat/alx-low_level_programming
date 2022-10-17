2008511a8eda1dbc6dc0f56e99b758f4859d69e5
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-17 14:22:39 -0700

    print hm

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print all combintion of 2 digit num from 0 to 99
+ * Return: 0 if successful
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 99, i++)
+	{
+		for (j = (i + 1); j <= 99, j++)
+		{
+			putchar(i / 10);
+			putchar(i % 10);
+			putchar(' ');
+			putchar(j / 10);
+			putchar(j % 10);
+
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
