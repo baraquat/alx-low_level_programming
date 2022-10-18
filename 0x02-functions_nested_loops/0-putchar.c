fd4e1d43dba2e4507cbfdeaf27fcbc264cc369d6
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-18 15:30:58 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * _putchar - print _putchar followed by a new line
+ * Return: 0 if successful
+ */
+int _putchar(void)
+{
+	char text[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
