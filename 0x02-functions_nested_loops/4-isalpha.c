c03f49657a99dd486cbfca6ba0bb0678ecaca555
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-18 17:01:45 -0700

    check c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isalpha - check for alphabetic character c
+ * @c: alphabetic chater to be chacked
+ * Return: 1 if successful, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		if (c > 'A' && c < 'Z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
