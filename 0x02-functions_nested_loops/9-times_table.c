ccb76c4fbb18db83079def725650609634da8d20
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-19 07:18:59 -0700

    print 9 timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,12 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
+		_putchar(0);
 		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			_putchar(k / 10);
-			_putchar(k % 10);
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
