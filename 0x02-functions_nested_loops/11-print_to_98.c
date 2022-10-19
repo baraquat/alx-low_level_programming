875530f50f84f4dd0fd1b32d11b1df9c062f9454
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-19 11:02:46 -0700

    print n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,19 +11,15 @@ void print_to_98(int n)
 	{
 		for (n = n; n < 98; n++)
 		{
-			_putchar(n);
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
 	}
 	else if (n > 98)
 	{
 		for (n = n; n > 98; n--)
 		{
-			_putchar(n);
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
 	}
-	_putchar('\n');
+	printf("98\n");
 }
