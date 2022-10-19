564519cb85b6591939d0c0a67f51837129f34464
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-19 06:05:47 -0700

    print last dogot

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,9 @@ int print_last_digit(int n)
 	digit = n % 10;
 
 	if (n < 0)
-	{
-		digit = digit * -1;
-		_putchar(digit + '0');
-	}
+	digit = (digit * -1);
+
+	_putchar(digit + '0');
+
 	return (digit);
 }
