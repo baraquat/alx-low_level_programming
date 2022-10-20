ebee8bb8d305802e9ef88fbf32691008e1270642
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-20 05:50:35 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiply 2 integers
+ * @a: right operand
+ * @b: left operand
+ * Return: 0 if successful
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
