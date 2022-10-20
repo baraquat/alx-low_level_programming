fbb2764da7071d1ad241a66ec2621ad5282484e6
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-20 09:20:34 -0700

    print 0-p except 2,4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
  * print_most_numbers - print 0-9 except 2 and 4
  * Return: void
  */
+
 void print_most_numbers(void)
 {
 	int i;
@@ -10,7 +12,7 @@ void print_most_numbers(void)
 	for (i = '0'; i <= '9'; i++)
 	{
 		if (i != '2' && i != '4')
-			_putchar(i);
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 }
