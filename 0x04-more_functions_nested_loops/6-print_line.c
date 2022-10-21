1b9f6a3af176a079d9f202cddec60642c81914ff
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-21 15:20:56 -0700

    print a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in the terminal
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0);
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
