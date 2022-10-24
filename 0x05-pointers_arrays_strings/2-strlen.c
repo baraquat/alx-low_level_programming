a292807e3034ababecca201f26d3dd0fab9a7312
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-24 13:01:32 -0700

    return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: a pointer to an int that will be updated
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int k;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+		return (k);
+	}
+}
