61b5221f0bc047876819e4712b43cda0d916c5e8
Barakat Wahab <barakatwahab01@gmail.com>
2022-10-24 16:45:53 -0700

    print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_rev - function that prints a string, in reverse, followed by a new line
+ * print_rev - function that prints a string in reverse
  * @s: a pointer to an integer that will be updated
  * Return: void
  */
